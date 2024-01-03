318d0bada2651505f442f4d6f6b75b151f419183
GaneshMike <38181022+GaneshMike@users.noreply.github.com>
2024-01-03 14:54:11 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,13 +20,11 @@ st.markdown("""
 with st.sidebar:
     st.title('🤖💬 OpenAI Chatbot')
     openai.api_key = st.text_input('Enter OpenAI API token:', type='password')
-
+    
+    if not (openai.api_key.startswith('sk-') and len(openai.api_key)==51):
+        st.warning('Please enter your credentials!', icon='⚠️')
     else:
-        openai.api_key = st.text_input('Enter OpenAI API token:', type='password')
-        if not (openai.api_key.startswith('sk-') and len(openai.api_key)==51):
-            st.warning('Please enter your credentials!', icon='⚠️')
-        else:
-            st.success('Proceed to entering your prompt message!', icon='👉')
+        st.success('Proceed to entering your prompt message!', icon='👉')
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
